6c8888fdf064c0c6390b318f55418d5579b3792e
mpprom <sweeppers@gmail.com>
2021-10-31 00:37:38 +0700

    cardinal

---
+++ controllers/user.controller.js
@@ -0,0 +1,37 @@
+var express = require('express');
+const baseResponse = require('../helpers/base-response.helper');
+const usrModel = require('../model/usr');
+const result = {};
+result.getUserPage = async (req, res) => {
+    let mysql = null;
+     baseResponse.success = false;
+            baseResponse.responseCode = 501;
+            baseResponse.message = 'Data not found';
+ 
+    try {
+        const rsDetail = await usrModel.getTypesFromDB();
+    
+       
+        if (rsDetail.data) {
+           
+            
+            baseResponse.data = rsDetail.data;
+            baseResponse.success = true;
+            baseResponse.responseCode = 200;
+        } else {
+            baseResponse.success = false;
+            baseResponse.responseCode = 501;
+            baseResponse.message = 'Data not found';
+        }
+    } catch (error) {
+        console.log(`service index error : ${error}`);
+    } finally {
+        if (mysql) {
+            await mysql.release();
+        }
+    }
+     res.send(baseResponse);
+};
+
+//export default result;
+module.exports = result;
