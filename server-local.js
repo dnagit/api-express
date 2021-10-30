1cbf21c5db11922bfdc416db03c948eeb74b9796
mpprom <sweeppers@gmail.com>
2021-10-30 19:49:17 +0700

    api

--- server-local.js
+++ server-local.js
@@ -2,4 +2,4 @@
 
 const app = require('./express/server');
 
-app.listen(3001, () => console.log('Local app listening on port 3001!'));
+app.listen(3001, () => console.log('Local app listening on port 3000!'));
